93f601995c85ce5bdc9ccb112de86dcf059cd72f
TiasySeva <stutichaurasia4238@gmail.com>
2023-09-25 19:00:49 +0530

    1ST C++ Program to print Namaste Dunia

---
+++ p1.cpp
@@ -0,0 +1,13 @@
+// First C++ Program
+// To print Namaste Dunia
+
+# include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Namaste Dunia :-)" <<endl;
+    cout << "Namaste Dunia :-)\n";
+    cout << "Namaste Dunia :-)";
+    
+}
