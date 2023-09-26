bb9a2af128501c273e0371c9b2cd2f279077dda1
TiasySeva <stutichaurasia4238@gmail.com>
2023-09-26 17:41:05 +0530

    Added concept of signed

--- p4.cpp
+++ p4.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 int main()
 {
+    signed int i =-112;
+    cout<<i<<endl;
+
+    
+    signed int i1 =112;
+    cout<<i1<<endl;
+
+    
+    signed i2 =-112;
+    cout<<i2<<endl;
+
     //unsigned stores only positive nos 
     unsigned int a =112;
     cout<<a<<endl;
+
+    
+    unsigned i3 =112;
+    cout<<i3<<endl;
 
     //it will get a very large no as output
     /*Compiler will try to print 2's complement but since the value is unsigned,
