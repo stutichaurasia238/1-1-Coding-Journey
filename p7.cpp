47f31d44fb217fbecca89bf2c2b03348ce6bcd42
TiasySeva <stutichaurasia4238@gmail.com>
2023-09-26 19:18:44 +0530

    Program for Relational and Logical operator

---
+++ p7.cpp
@@ -0,0 +1,20 @@
+//relational operator < , > , <= , >= , = , !=
+//logical operator && , || , !
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+a=5;
+b=6;
+bool first = (a==b);
+cout<< first <<endl;
+
+// any no except is considered as 1 and not 1 is 0
+int a1=21;
+cout<<!a1<<endl;
+
+int a2=0;
+cout<<!a2<<endl;
+}
+
