8398b56647dcc6b706047ef989c999bd79855abd
TiasySeva <stutichaurasia4238@gmail.com>
2023-09-26 19:08:50 +0530

    Program to handle default precision using printf

---
+++ p6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     // arithmetic operator (+ - * / %)
+    // 2 printed because 2.0 is converted to 2
+    float a =4.0 /2.0;
+    cout <<a<<endl;
+
+    printf("%f\n",a);
+    printf("%.1f\n",a);// Display with 1 decimal place
+}
